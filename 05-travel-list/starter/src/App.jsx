c981f193276d0c5dad53b099c5c885349fff5038
realFK98 <wishira2013.coth@gmail.com>
2024-06-15 20:30:55 +0700

    Update App.jsx

--- 05-travel-list/starter/src/App.jsx
+++ 05-travel-list/starter/src/App.jsx
@@ -1,19 +1,33 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Socks", quantity: 12, packed: false },
-  { id: 4, description: "Socks", quantity: 12, packed: false },
-  { id: 5, description: "Socks", quantity: 12, packed: false },
-];
-
 export default function App() {
+  const [items, setItems] = useState(
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false }
+  );
+  function handleDelete(itemId) {
+    setItems((items) => items.filter((item) => item.id !== itemId));
+  }
+  function handleAddItem(newItem) {
+    setItems((items) => [...items, newItem]);
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) => {
+        return item.id === id ? { ...item, packed: !item.packed } : item;
+      })
+    );
+  }
   return (
     <div className="h-screen">
       <Logo />
-      <Form />
-      <PackingLinst />
+      <Form onAddItem={handleAddItem} />
+      <PackingLinst
+        onToggleItem={handleToggleItem}
+        onDeleteItem={handleDelete}
+        items={items}
+      />
       <Status />
     </div>
   );
@@ -23,15 +37,15 @@ function Logo() {
   return <h1>Far Away💀</h1>;
 }
 
-function Form() {
+function Form({ onAddItem }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
     if (!description) return;
-    const newItem = { description, quantity, package: false, id: Date.now() };
-    console.log(newItem);
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    onAddItem(newItem);
     setDescription("");
     setQuantity(1);
   }
@@ -56,25 +70,31 @@ function Form() {
   );
 }
 
-function PackingLinst() {
+function PackingLinst({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list ">
       <ul className="overflow-hidden">
-        {initialItems.map((item) => (
-          <Item key={item.id} item={item} />
+        {items.map((item) => (
+          <Item
+            onToggleItem={onToggleItem}
+            onDeleteItem={onDeleteItem}
+            key={item.id}
+            item={item}
+          />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
-      <span>
+      <input type="checkbox" onChange={() => onToggleItem(item.id)} />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
