1cb8731fc773e755a00f43dd2f988feb41237555
realFK98 <wishira2013.coth@gmail.com>
2024-07-31 22:16:18 +0700

    add fake api  and start quiz react project

--- 10-react-quiz/starter/src/App.jsx
+++ 10-react-quiz/starter/src/App.jsx
@@ -1,8 +1,37 @@
-import { useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import Header from "./Header.jsx";
 import MainWeb from "./MainWeb.jsx";
 // import DateCounter from "./DateCounter.jsx";
+
+const initialState = {
+  questions: [],
+  // loading error ready active finished
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("http://localhost:8000/questions").catch((err) =>
+        dispatch({ type: "dataFailed" })
+      );
+      const data = await res.json();
+      dispatch({ type: "dataReceived", payload: data });
+    })();
+  }, []);
+
   return (
     <div className="heappader">
       <Header />
